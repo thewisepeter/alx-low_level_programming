bb0682eb2a71e6fa0374353b3c79af9610a6a580
Peter Ochan <rempeterwisdom@gmail.com>
2023-02-17 08:57:28 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,12 +19,13 @@ int main(void)
 	 * find last digit by dividing by ten and recording the remainder
 	 * write result to stdout
 	 */
-	last_digit = (&n % 10);
+	last_digit = (n % 10);
 	if (last_digit > 5)
-		printf("%d %d and is greater than 5", last_digit, n);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 	if (last_digit == 0)
-		printf("%d %d and is 0", last_digit, n);
+		printf("Last digit of %d is %d and is 0\n", n, last_digit);
 	if ((last_digit < 6) && (last_digit != 0))
-		printf("%d %d and is less than 6 and not 0", last_digit, n);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",
+				n, last_digit);
 	return (0);
 }
