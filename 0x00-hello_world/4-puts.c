ef04828467132e06fb0f7c65bdc119af59b4b594
Peter Ochan <rempeterwisdom@gmail.com>
2023-02-16 16:09:00 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/**
+	 *string initialisation
+	 */
+	char my_string[] = "\"Programming is like building a multilingual puzzle";
+
+	/**
+	 *writing the string to stdout
+	 */
+	puts(my_string);
+	return 0;
+}
