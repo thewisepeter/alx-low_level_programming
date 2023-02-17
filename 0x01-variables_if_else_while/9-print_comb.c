bc805345caabf7ef97951f8cb1b4d081cad2a1d6
Peter Ochan <rempeterwisdom@gmail.com>
2023-02-17 14:42:37 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of single digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10 ; n++)
+	{
+		/**
+		 * writes number  to stdout
+		 */
+		if (n < 10)
+		putchar(n + '0');
+		/**
+		 * adds comma and space and writes to stdout
+		 */
+		if (n < 9)
+		putchar (',');
+		putchar (' ');
+	}
+	putchar ('\n');
+	return (0);
+}
