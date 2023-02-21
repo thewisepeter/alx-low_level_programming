e3103a32faa5df378c55ce210660b0569e28ee6f
Peter Ochan <rempeterwisdom@gmail.com>
2023-02-21 13:56:28 -0800

    Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,15 +12,15 @@ void jack_bauer(void)
 	 */
 	int a, b, c, d;
 
-	for (a = 0 ; a < 3 ; a++)
+	for (a = 0 ; a <= 2 ; a++)
 	{
-	for (b = 0 ; b < 9 ; b++)
+	for (b = 0 ; b <= 9 ; b++)
 	{
 	if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 	{
-	for (c = 0 ; c < 6 ; c++)
+	for (c = 0 ; c <= 5 ; c++)
 	{
-	for (d = 0 ; d < 10 ; d++)
+	for (d = 0 ; d <= 9 ; d++)
 	{
 	_putchar (a + '0');
 	_putchar (b + '0');
