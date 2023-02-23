47b53f61dd50bbff1fea2b230c8913c59659db37
Peter Ochan <rempeterwisdom@gmail.com>
2023-02-23 14:43:22 -0800

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - FizzBuzz entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 1 ; c <= 100; c++)
+	{
+		if ((c % 15) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((c % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else if ((c % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", c);
+		}
+
+	}
+	return (0);
+}
