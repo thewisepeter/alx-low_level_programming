2f6ae41bba7b7034d2193b1ca6e91e080747672b
Peter Ochan <rempeterwisdom@gmail.com>
2023-02-25 06:44:12 -0800

    50 first fibo

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * prints the first 50 Fibonacci numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int i, n;
+	long a = 1, b = 2;
+	int sum  = a + b;
+
+	while (i < 50)
+	{
+		if (i == 0)
+			printf("%ld", a);
+		else if (i == 1)
+			printf(", %ld", b);
+		else
+		{
+			b = b + a;
+			a = b - a;
+		}
+		++i;
+	}
+	printf("\n");
+	return (0);
+}
