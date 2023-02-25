c84e653faba1a1d79758a473342d7f740df1af13
Peter Ochan <rempeterwisdom@gmail.com>
2023-02-25 04:36:30 -0800

    prints the n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -26,7 +26,7 @@ void print_times_table(int n)
 					_putchar(' ');
 				if (p >= 100)
 				{
-					_putchar((p / 100) * '0');
+					_putchar((p / 100) + '0');
 					_putchar(((p / 10)) % 10 + '0');
 				}
 				else if (p <= 99 && p >= 10)
