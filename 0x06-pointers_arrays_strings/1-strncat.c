d1a2832dc2b39733f767b36b2c53cb4e60783cbb
Peter Ochan <rempeterwisdom@gmail.com>
2023-03-01 09:43:17 -0800

    function that concatenates two strings use at most n bytes from src

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len = 0;
+	int src_len = 0;
+
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
+
+	while (src[src_len] != '\0' && src_len < n)
+	{
+		dest[dest_len + src_len] = src[src_len];
+		src_len++;
+	}
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/number1.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - function that concatenates two strings
+ * @src: source file
+ * @dest: destination file
+ * @n: bytes used of src
+ * Return: pointer
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+	n = 0;
+
+	for (i = 0 ; dest[i] != 0 ; i++)
+	{
+		;
+	}
+	while (j != (n))
+	{
+		for (j = 0 ; src[j] != 0 ; j++)
+		{
+			dest[i] = src[j];
+			i++;
+		}
+	}
+	dest[i] = src[j];
+	return (dest);
+}
