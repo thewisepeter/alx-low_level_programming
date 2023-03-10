1b0a7da210cf6db8ea5dade1064cd8a753d4c748
Peter Ochan <rempeterwisdom@gmail.com>
2023-03-09 16:23:48 -0800

    program that prints all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,22 +14,24 @@ int main(void)
 
 	for (a = 0 ; a < 100 ; a++)
 	{
-		for (b = a + 1 ; b < 10 ; b++)
+		for (b = 0 ; b < 10 ; b++)
 		{
-			putchar ((a / 10) + '0');
-			putchar ((a % 10) + '0');
-			putchar (' ');
-			putchar ((b / 10) + '0');
-			putchar ((b % 10) + '0');
-
-			if (a == 98 && b == 99)
-				putchar('\n');
-			else
+			if (a < b)
 			{
-				putchar (',');
+				putchar ((a / 10) + '0');
+				putchar ((a % 10) + '0');
 				putchar (' ');
+				putchar ((b / 10) + '0');
+				putchar ((b % 10) + '0');
+
+				if (a != 98 || b != 99)
+				{
+					putchar (',');
+					putchar (' ');
+				}
 			}
 		}
 	}
+	putchar('\n');
 	return (0);
 }
