95aa04831c8469e22b63f1fdb01a6a07c8aecb2e
Peter Ochan <rempeterwisdom@gmail.com>
2023-03-11 00:37:43 -0800

    program that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,47 +1,16 @@
 #include "main.h"
-#include <unistd.h>
-
-void _puts(char *str);
-int _putchar(char c);
+#include <stdio.h>
 
 /**
  * main - program that prints its name
  * @argc: number of arguments passed to main
  * @argv: string
  *
- * Return: 0 (Success)
+ * Return: 0 (success)
  */
 int main(int argc, char *argv[])
 {
 	if (argc > 0)
-		_puts(argv[0]);
+		printf("%s\n", argv[0]);
 	return (0);
-}
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- * _puts - function that prints a string, followed by a new line, to stdout
- * @str: str to be outputted
- * Return: void
- */
-void _puts(char *str)
-{
-	int i;
-
-	for (i = 0 ; str[i] != '\0' ; ++i)
-	{
-		_putchar(str[i]);
-	}
-	_putchar('\n');
 }
