ca1b4fdf0d449466e92c5e70c491c99691af3e97
Peter Ochan <rempeterwisdom@gmail.com>
2023-03-12 11:35:33 -0700

    prints the minimum number of coins to make change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,10 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <string.h>
-
-int min_val(int a[], int b);
 
 /**
  * main - program that prints the minimum number
@@ -16,58 +12,32 @@ int min_val(int a[], int b);
  */
 int main(int argc, char *argv[])
 {
-	int coins[5] = {1, 2, 5, 10, 25};
-	int b;
+	int coins[5] = {25, 10, 5, 2, 1};
+	int val, i, res;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	
-	b = atoi(argv[1]);
-	if (b < 0)
+
+	val = atoi(argv[1]);
+	res = 0;
+
+	if (val < 0)
 	{
 		printf("0\n");
+		return (0);
 	}
-	else
+
+	for (i = 0 ; i < 5 && val >= 0 ; i++)
 	{
-		printf("%d\n", min_val(coins[5], b));
-	}
-	return (0);
-}
-
-/**
- * min_val - program that prints the minimum number
- * of coins to make change for an amount of money
- * @a: array of coins
- * @n: maximum number of coins given
- * @b: value whose changed is required
- *
- * Return: min_val
- */
-int min_val(int a[], int b)
-{
-	int i, j;
-
-	i = 0;
-	j = 0;
-	int f[i][j];
-
-	f[i][0] = 0;
-	for (i = 0 ; i <= sizeof(a) ; i++)
-	{
-		for (j = 0 ; j < b ; j++)
+		while (val >= coins[i])
 		{
-			if (a[i] > j)
-			{
-				f[i][j] = f[i - 1][j];
-			}
-			else
-			{
-				f[i][j] = min(f[i - 1][j], 1 + f[i][j - a[i]]);
-			}
+			res++;
+			val -= coins[i];
 		}
 	}
-	return (f[i][j]);
+	printf("%d\n", res);
+	return (0);
 }
