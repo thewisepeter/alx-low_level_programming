ba1cac93a5a473744bbf043311a38d061c1322b7
Peter Ochan <rempeterwisdom@gmail.com>
2023-03-16 13:33:16 -0700

    function that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,31 +11,33 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
-	unsigned int len1, len2, len, i;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
-	len1 = 0;
-	while (s1[len1] != '\0')
+	while (s1 && s1[len1])
 		len1++;
-	len2 = 0;
-	while (s2[len2] != '\0')
-		len2++;
-	len = (n < len2) ? (len1 + n) : (len1 + len2);
 
-	str = malloc(sizeof(char) * (len + 1));
+	while (s2 && s2[len2])
+		len2++;
+
+	if (n < len2)
+		str = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		str = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (str == NULL)
 		return (NULL);
 
-	for (i = 0; i < len1 ; i++)
+	while (i < len1)
+	{
 		str[i] = s1[i];
-	for (i = 0 ; i < n && s2[i] != '\0' ; i++)
-		str[len1 + i] = s2[i];
-	for (i = 0 ; i < len2 && s2[i] != '\0' ; i++)
-		str[len1 + i] = s2[i];
-	str[len] = '\0';
+		i++;
+	}
+
+	while (n < len2 && i < (len1 + n))
+		str[i++] = s2[j++];
+
+	while (n >= len2 && i < (len1 + len2))
+		str[i++] = s2[j++];
+
+	str[i] = '\0';
 	return (str);
 }
