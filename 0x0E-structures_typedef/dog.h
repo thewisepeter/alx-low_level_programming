97b75dcac7aa522274d580aee9e8d325da5f274c
Peter Ochan <rempeterwisdom@gmail.com>
2023-03-20 13:50:10 -0700

    struct that takes dogs name age and owner's name

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - takes dogs name age and owner's name
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's details
+ *
+ * Description: takes dogs name age and owner's name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
