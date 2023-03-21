e6e6cf40a84a3f6c4ff906fe832c8d2a55d82d75
Peter Ochan <rempeterwisdom@gmail.com>
2023-03-21 02:27:15 -0700

    function that frees memory allocate    d to dog_t *d

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - function that frees memory allocated to dog_t *d
+ * @d: pointer to struct
+ *
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
