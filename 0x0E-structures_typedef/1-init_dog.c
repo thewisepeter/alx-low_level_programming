dfa4985578310811bccad8c3a5262875b141bf1f
Peter Ochan <rempeterwisdom@gmail.com>
2023-03-24 00:28:40 -0700

    fixed commenting for #endif in dog.h

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,7 +13,8 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-		d = malloc(sizeof(struct dog));
+		d = return;
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
