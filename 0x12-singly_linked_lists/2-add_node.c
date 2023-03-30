43ef179eb8184cb629055618581ee0ac6377840b
Peter Ochan <rempeterwisdom@gmail.com>
2023-03-30 09:10:07 -0700

    fixed memory leak in 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -20,12 +20,6 @@ list_t *add_node(list_t **head, const char *str)
 	new_node = malloc(sizeof(list_t));
 	if (!new_node)
 		return (NULL);
-	new_node->str = malloc(sizeof(char) * (len + 1));
-	if (!new_node->str)
-	{
-		free(new_node);
-		return (NULL);
-	}
 
 	new_node->str = strdup(str);
 	new_node->len = len;
