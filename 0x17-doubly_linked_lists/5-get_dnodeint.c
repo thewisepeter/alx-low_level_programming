265bdc2e3552070e5b12f0b68427b32c5bf385f9
Peter Ochan <rempeterwisdom@gmail.com>
2023-05-18 13:52:49 +0300

    function that returns the nth node of a DLL

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - function that returns the nth node of a DLL
+ * @head: DLL
+ * @index: index of node
+ *
+ *
+ * Return: pointer to node (Success) or NULL (fail)
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	dlistint_t *current;
+	
+	if (!head)
+		return (NULL);
+
+	current = head;
+
+
+	while (current)
+	{
+		if (index == i)
+			return (current);
+		i++;
+		current = current->next;
+	}
+
+	return (NULL);
+}
