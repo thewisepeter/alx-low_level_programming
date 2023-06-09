cdc30f523e4bf1686066fe20ee91234a265a43ed
Peter Ochan <rempeterwisdom@gmail.com>
2023-06-09 13:14:51 +0300

    worked logic for size array

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,25 +9,21 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
-	hash_node_t *node;
 
 	hash_table = malloc(sizeof(hash_table_t));
 	if (!hash_table)
 		return (NULL);
 
-	node = malloc(sizeof(hash_node_t));
-	if (!hash_table)
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (!hash_table->array)
 	{
 		free(hash_table);
 		return (NULL);
 	}
 
-	node->key = NULL;
-	node->value = NULL;
-	node->next = NULL;
-
 	hash_table->size = size;
-	hash_table->array = &node;
+	for (i = 0; i < size; i++)
+		hash_table->array[i] = NULL;
 
 	return (hash_table);
 }
