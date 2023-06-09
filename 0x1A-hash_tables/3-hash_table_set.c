5f47c0db5cacb3792cd816424cb76ac5687502ad
Peter Ochan <rempeterwisdom@gmail.com>
2023-06-09 21:50:19 +0300

    added substitution for similar keys

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,22 +10,42 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	char *value_copy;
 	const unsigned char *key_1 = (const unsigned char *)key;
-
 	unsigned long int i = key_index(key_1, ht->size);
-	hash_node_t *node = malloc(sizeof(hash_node_t));
+	hash_node_t *node;
 
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (0);
+
+	value_copy = strdup(value);
+	if (!value_copy)
+		return (0);
+
+	for (; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
+	}
+
+	node = malloc(sizeof(hash_node_t));
 	if (!node)
+	{
+		free(value_copy);
 		return (0);
-
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-		return (0);
-
+	}
 	node->key = strdup(key);
-	if (value == NULL)
-		node->value = NULL;
-	else
-		node->value = strdup(value);
+	if (!node->key)
+	{
+		free(value_copy);
+		free(node);
+		return (0);
+	}
+	node->value = value_copy;
 	node->next = NULL;
 
 	if (ht->array[i] == NULL)
