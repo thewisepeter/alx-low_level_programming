327ec6d629b27e15d7ea8e58b5e1e5f8f02d7c04
Peter Ochan <rempeterwisdom@gmail.com>
2023-06-09 23:44:22 +0300

    added loop to tranverse incase of linked list

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -20,7 +20,13 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	index = key_index((const unsigned char *)key, ht->size);
 	current = ht->array[index];
 
-	if (current != NULL)
-		return (current->value);
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			return (current->value);
+		}
+		current = current->next;
+	}
 	return (NULL);
 }
